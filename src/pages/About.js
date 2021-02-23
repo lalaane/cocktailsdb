3937ddc3f427b4d6160d2591f18a5eb4f9d58c35
Laura Laane <laura.laane@gmail.com>
2021-02-23 23:05:57 +0200

    initial commit

---
+++ src/pages/About.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const About = () => {
+	return (
+		<section className='section about-section'>
+			<h1 className='section-title'>about us</h1>
+			<p>
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident fugit accusantium animi. Id voluptate facilis
+				quaerat minima ex rerum consequuntur sit aliquid, recusandae ad animi architecto perferendis cumque dicta neque!
+			</p>
+		</section>
+	);
+};
+
+export default About;
